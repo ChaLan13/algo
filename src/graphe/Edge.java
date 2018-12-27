5efbf810b41cded2db7dd329b0e9254382227a96
Charlotte LANUEL <charlotte.lanuel@hotmail.fr>
2018-12-27 17:53:36 +0100

    code prof corrigé pour éviter les erreurs

---
+++ src/graphe/Edge.java
@@ -0,0 +1,48 @@
+package graphe;
+
+public class Edge
+{
+   private int from;
+   private int to;
+   private boolean used;
+
+
+   public Edge(int x, int y)
+    {
+        this.from = x;
+        this.to = y;
+        this.used = false;
+    }
+
+    public final int other(int v)
+    {
+	    if (this.from == v)
+	        return this.to;
+	    else
+	        return this.from;
+    }
+
+    public int getFrom() {
+        return from;
+    }
+
+    public void setFrom(int from) {
+        this.from = from;
+    }
+
+    public int getTo() {
+        return to;
+    }
+
+    public void setTo(int to) {
+        this.to = to;
+    }
+
+    public boolean isUsed() {
+        return used;
+    }
+
+    public void setUsed(boolean used) {
+        this.used = used;
+    }
+}

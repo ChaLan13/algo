dd7611896ba7c039b05661600b79be4f819f4466
Charlotte LANUEL <charlotte.lanuel@hotmail.fr>
2019-01-02 16:04:40 +0100

    debut kruskal
    d'après l'énoncé il serait bien de faire une structure unio-find, mais où et comment ?

---
+++ src/algos/Kruskal.java
@@ -0,0 +1,40 @@
+package algos;
+
+import graphe.Edge;
+import graphe.Graph;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Kruskal {
+
+    private Graph g;
+
+    public Kruskal(Graph graph){
+        g = graph;
+    }
+
+    public ArrayList<Edge> executerAlgo() {
+        ArrayList<Edge> aretes = g.edges();
+        ArrayList<Edge> res = new ArrayList<Edge>();
+
+        Collections.shuffle(aretes);
+
+        do {
+            res.add(aretes.get(0));
+            aretes.remove(0);
+
+        } while (!estCyclique(res));
+        return res;
+    }
+
+
+    public boolean estCyclique(ArrayList<Edge> list){
+        Edge areteATester = list.get(list.size()-1);
+        for(int i = 0; i < list.size()-1; i++){
+            if(areteATester.getTo() == list.get(i).getFrom())
+
+        }
+
+    }
+}

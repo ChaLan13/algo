f38f21c1e50726a1961be8b91ff11f625d52d44d
Lukyky414 <lucassschwab.e@gmail.com>
2019-01-03 17:46:43 +0000

    Tient cha

--- tmp.java
+++ tmp.java
@@ -1,55 +1,67 @@
-public class ArbreCouvrant {
-	public List<Edge> aretes;
+public int executerAlgo(){
+	int size = g.vertices();
+	boolean[] points_visites = new boolean[size]();
+	ArrayList<Edge> res = new ArrayList<>();
+	Random r = new Random();
 
-	public ArbreCouvrant(List<Edge> ar){
-		this.aretes = ar;
-	}
+	int pointActuel = r.nextInt(size);
+	points_visites[pointActuel] = true;
+	size--;
 
-	public boolean equals(List<Edge> ar){
-		int size = ar.size();
-		for(int i=0; i < size; i++){
-			Edge e = ar.get(i);
-			if(!aretes.contains(e))
-				return false;
+	while(size > 0){
+
+		boolean arreter = false;
+
+		ArrayList<Edge> adj = g.adj(pointActuel);
+		adj.shuffle();
+Â		Edge e;
+		for(int i=0; (i < adj.size()) && !arreter; i++){
+			e = adj.get(i);
+
+			if(e.isUsed()){//On peut retourner en arriere
+				//car l arete est deja visitee
+				//mais on n ajoute rien aux points visites
+				arreter = true;
+			}
+			else{
+				/*Comme le graphe n'est pas considere comme oriente,
+				* Il faut donc essayer dans le sens from -> to
+				* et dans le sens to -> from
+				*/
+				e.setUsed(true);
+				res.add(e);
+
+				//from -> to
+				if(e.getFrom() == pointActuel){
+					if(!points_visites[e.getTo()]){
+						//Si le point est deja visite, Ne pas prendre car creation d'un cycle
+						//mais la on sait qu'il n'est pas visite, donc on va le visiter
+						points_visites[e.getTo()] = true;
+						size--;
+						arreter = true;
+					}
+				}
+				else{//To -> From
+					if(!points_visites[e.getFrom()]){
+						arreter = true;
+						points_visites[e.getFrom()] = true;
+						size--;
+					}
+				}
+			}
 		}
-		return true;
+
+		if(!arreter) // Si on ne s'est pas arrete volontairement (donc pas trouve de chemin)
+			//alors error
+			throw new Exception("Graphe avec un point d'arite 0");
+
+		//Visiter l'arrete
+		if(e.getFrom() == pointActuel) //sens From -> To
+			pointActuel = e.getTo();
+		else// To -> From
+			pointActuel = e.getFrom();
+
 	}
 
-}
-
-
-//Kruskal
-
-ArrayList<ArbreCouvrant> abres;
-int size;
-
-kruskal(Graph g){
-	//...
-	arbres = new ArrayList<>();
-
-	ArrayList<Edge> abre = new ArrayList<>();
-	arbre.add();
-	//....
-	arbres.add(new ArbreCouvrant(arbre));
-
-	arbre = new ArrayList<>();
-	arbre.add();
-	//...
-	arbres.add(new ArbreCouvrant(arbre));
-
-	size = 8;
-}
-
-
-int executeralgo(){
-	/*
-	tout algo
-	*/
-
-	for(int i = 0; i < this.size; i++){
-		ArbreCouvrant a = arbres.get(i);
-		if(a.equals(res))
-			return i;
-	}
-	return -1;
+	//verif avec abrecouvrant;
 }
